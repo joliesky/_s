076bb1fd0a8edb94a9fde36387fdb94aa04e191f
joliesky <jolie.mccullough@gmail.com>
2013-08-28 21:17:39 -0600

    Changed all _s instances with jolie_theme

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
  *
  * Contains the closing of the id=main div and all content after
  *
- * @package _s
+ * @package Jolie_theme
  */
 ?>
 
@@ -12,10 +12,10 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<?php do_action( '_s_credits' ); ?>
-			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', '_s' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', '_s' ), 'WordPress' ); ?></a>
+			<?php do_action( 'jolie_theme_credits' ); ?>
+			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'jolie_theme' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'jolie_theme' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', '_s' ), '_s', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'jolie_theme' ), 'jolie_theme', '<a href="http://joliemccullough.com/" rel="designer">Jolie McCullough</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
