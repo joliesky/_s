3e66b04abd4813e2a150872c8f02800e0376dffe
joliesky <jolie.mccullough@gmail.com>
2013-08-28 22:10:47 -0600

    quick style fixes, ready to download for local server

--- footer.php
+++ footer.php
@@ -7,17 +7,17 @@
  * @package Jolie_theme
  */
 ?>
+	</div><!-- .container -->
+		</div><!-- #content -->
 
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<?php do_action( 'jolie_theme_credits' ); ?>
-			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'jolie_theme' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'jolie_theme' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'jolie_theme' ), 'jolie_theme', '<a href="http://joliemccullough.com/" rel="designer">Jolie McCullough</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			<div class="site-info">
+				<?php do_action( 'jolie_theme_credits' ); ?>
+				<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'jolie_theme' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'jolie_theme' ), 'WordPress' ); ?></a>
+				<span class="sep"> | </span>
+				<?php printf( __( 'Theme: %1$s by %2$s.', 'jolie_theme' ), 'jolie_theme', '<a href="http://joliemccullough.com/" rel="designer">Jolie McCullough</a>' ); ?>
+			</div><!-- .site-info -->
+		</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
